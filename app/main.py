38cd62bbd3f1bfb17531d394515d05b2bca99a58
LidTleJao <praphunpongtke@gmail.com>
2023-08-19 01:04:32 +0700

    first commit

---
+++ app/main.py
@@ -0,0 +1,39 @@
+import base64
+import pickle
+
+
+import numpy as np
+import requests
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+# use docker
+from app.code import predict_brand
+
+# from code import predict_brand
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# model = pickle.load(open(r'C:\Users\User\Desktop\AI\Assignment1\CarBrandClass\model\model_XGB.pkl','rb'))
+# use docker
+model = pickle.load(open(f'model/carbrandmodel.pkl','rb'))
+
+# end_hog = 'http://localhost:8080/api/gethog'
+# use docker
+end_hog = 'http://172.17.0.1:80/api/gethog'
+
+@app.get("/")
+def root():
+    return {"message": "This is my api"}
+
+@app.post("/api/carbrand")
+async def read_str(request:Request):
+    item = await request.json()
+    hog = requests.get(end_hog,json=item)
+    res = predict_brand(model,hog.json()['HOG'])
+    return res
